2683dde04eff37d30e598272019e5e6cabc93a1f
Maizares, Juan <juan_maizares386@hotmail.com>
2022-08-27 17:03:58 -0300

    useContext

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,22 @@
+import { Navigate, Routes, Route, Link } from 'react-router-dom';
+import { UserProvider } from '../context/UserProvider';
+import { AboutPage } from './AboutPage';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+import { Navbar } from './Navbar';
+
+export const MainApp = () => {
+    return (
+        <UserProvider>
+            <Navbar />
+            <hr />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='login' element={<LoginPage />} />
+                <Route path='about' element={<AboutPage />} />
+                
+                <Route path='/*' element={<Navigate to="/about" />} />
+            </Routes>
+        </UserProvider>
+    )
+}
